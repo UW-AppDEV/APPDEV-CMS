51e03f0578fd48ce4db146593f6a19c843cf37c1
Khashayar Pourdeilami <kpourdeilami@gmail.com>
2014-11-12 20:40:28 -0500

    added routing

--- js/app.js
+++ js/app.js
@@ -15,7 +15,40 @@ var localStorage = window.localStorage;
  Routing
  */
 
+app.config(function($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /404
+    $urlRouterProvider.otherwise("/404");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "partials/home.html",
+            controller: "homeCtrl"
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "partials/about.html",
+            controller: "aboutCtrl"
+        })
+        .state('events', {
+            url: "/events",
+            templateUrl: "partials/events.html",
+            controller: "eventsCtrl"
+        })
+        .state('admin', {
+            url: "/admin",
+            templateUrl: "partials/admin.html",
+            controller: "adminCtrl"
+        })
+        .state('404', {
+            url: '404',
+            templateUrl: "partials/404.html",
+            controller: "404Ctrl"
+        });
 
+});
 
 /*
  Services
